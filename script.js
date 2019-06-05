9fda9b5434c69e60a929b497141b789e75eeb762
aoelbaum <ashleyoelbaum@gmail.com>
2019-06-05 16:53:39 -0400

    bonus 1

--- script.js
+++ script.js
@@ -257,3 +257,32 @@
 // equipWeapon(Gandalf,'wizard staff');
 // Gandalf.describe();
 
+const HEROES = [
+  { id: 1, name: 'Captain America', squad: 'Avengers' },
+  { id: 2, name: 'Iron Man', squad: 'Avengers' },
+  { id: 3, name: 'Spiderman', squad: 'Avengers' },
+  { id: 4, name: 'Superman', squad: 'Justice League' },
+  { id: 5, name: 'Wonder Woman', squad: 'Justice League' },
+  { id: 6, name: 'Aquaman', squad: 'Justice League' },
+  { id: 7, name: 'Hulk', squad: 'Avengers' },
+];
+
+function findOne(arr, query){
+  let heroMatch = arr.filter(hero => {
+    let queryKeys= Object.keys(query);
+    for (let key of queryKeys){
+      if(hero[key] !== query[key])
+      return false;      
+    }
+    return true;
+  });
+  console.log(heroMatch);  
+  if(heroMatch.length > 0){
+    return heroMatch[0];
+  }
+  else {
+    return null;
+  }
+}
+
+console.log(findOne(HEROES, { squad: 'Justice League' }));

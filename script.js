63aaa512a68d033fb0b2694d8096114ecd0baa15
aoelbaum <ashleyoelbaum@gmail.com>
2019-06-05 12:30:19 -0400

    object drills 2

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 'use strict';
-function createMyObject() {
-  return {
-    foo: 'bar',
-    answerToUniverse : 42,
-    'olly olly' : 'oxen free',
-    sayHello: function() {
-      return 'hello';
-    }
-  };
+// function createMyObject() {
+//   return {
+//     foo: 'bar',
+//     answerToUniverse : 42,
+//     'olly olly' : 'oxen free',
+//     sayHello: function() {
+//       return 'hello';
+//     }
+//   };
+// }
+
+// let call = createMyObject();
+// console.log(call);
+
+function updateObject(obj) {
+  obj.foo = "foo";
+  obj.bar = "bar";
+  obj.bizz = "bizz";
+  obj.bang = "bang";
+  return obj;
 }
 
-let call = createMyObject();
-console.log(call);
+let testCall = updateObject()
